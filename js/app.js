43c4e891c722ba170b0ba123133b4cdb5bee1d58
Mario T. Lanza <mario@doesideas.com>
2023-01-18 10:43:29 -0500

    more partial application, doc updates

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 import _ from "./lib/atomic_/core.js"; //the trailing underscore in `atomic_` signifies functions are wrapped for build-free partial application
 import $ from "./lib/atomic_/reactives.js";
-import dom from "./lib/atomic_/dom.js"; //has some of its own reactives
+import dom from "./lib/atomic_/dom.js"; //includes its own reactives
 import t from "./lib/atomic_/transducers.js";
+import * as v from "./todos.js"; // grab the functional core, "v" for virtual
 
-import * as v from "./todos.js"; // this is the functional core and what follows...
-//...the imperative shell!
-
-//prepare to create certain kinds of elements
+//prepare to create elements
 const li = dom.tag('li'),
       label = dom.tag('label'),
       input = dom.tag('input'),
@@ -31,10 +29,10 @@ function todoItem(item){
   ];
 }
 
-// install app on matching elements, usually just one
-_.each(function(el){
+// locate potentially matching element
+_.maybe(dom.sel1("#todoapp"), function(el){
 
-  //locate key elements
+  //locate fixed, relative elements
   const entry = dom.sel1(".new-todo", el),
         list = dom.sel1(".todo-list"),
         filters = dom.sel1(".filters"),
@@ -42,7 +40,7 @@ _.each(function(el){
         count = dom.sel1(".todo-count strong"),
         all = dom.sel1("#toggle-all");
 
-  //define the required reactives
+  //setup necessary reactives
   const $state = $.cell(v.init()),
         $hash = dom.hash(window),
         $todo = $.map(_.get(_, "todo"), $state), //underscore is a partial application placeholder, note the other places this happens too
@@ -51,7 +49,7 @@ _.each(function(el){
         $total = $.map(_.count, $todo),
         $nothingDone = $.map(_.eq, $active, $total);
 
-  Object.assign(window, {$state}); //trick for choosing what to expose during interactive, REPL-driven development
+  Object.assign(window, {$state}); //trick for temporarily exposing choice objects for interactive, REPL-driven development
 
   function doneEditing(e){
     dom.removeClass(_.closest(e.target, "[data-id]"), "editing");
@@ -60,7 +58,7 @@ _.each(function(el){
 
   $.sub($state, _.log); //facilitates interactive development/debugging
 
-  //subscribe to events on reactives
+  //subscribe to reactives
   $.sub($hash, t.map(_.either(_, "#/")), function(hash){
     _.each(dom.removeClass(_, "selected"), views);
     dom.addClass(dom.sel1(`a[href='${hash}']`), "selected");
@@ -74,14 +72,14 @@ _.each(function(el){
   $.sub($active, dom.html(count, _));
   $.sub($nothingDone, dom.prop(all, "checked", _));
 
-  //subscribe to dom events almost always preferring delegates
+  //subscribe to dom preferring delegate events
   $.on(el, "dblclick", "[data-id]", function(e){
     dom.addClass(this, "editing");
     const tb = dom.sel1(".entry", this);
     tb.selectionStart = tb.selectionEnd = tb.value.length;
     tb.focus();
   });
-  $.on(el, "focusout", "[data-id].editing input.entry", doneEditing)
+  $.on(el, "focusout", "[data-id].editing input.entry", doneEditing);
   $.on(el, "keydown", "[data-id].editing input.entry", function(e){
     if (e.keyCode === 13){
       doneEditing(e);
@@ -105,5 +103,4 @@ _.each(function(el){
   $.on(el, "change", "li[data-id] input[type='checkbox']", function(e){
     _.swap($state, v.updateTodo(getId(e.target), "status", e.target.checked ? "completed" : "active"));
   });
-
-}, dom.sel(".todoapp")); //what designates the app
+});
